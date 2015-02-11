ba39e6635be17dd84cae02db5da3dd7ceb79cf06
Jussi Lahdenniemi <jussi@aprikoodi.fi>
2015-02-11 12:09:42 +0200

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,224 @@
 # cl-xmlw
-Simple namespace-aware XML writer for Common Lisp
+
+A simple namespace-aware XML writer for Common Lisp. Much like the
+xml-emitter project (http://www.cliki.net/xml-emitter), but with
+better support for characters with codes over 255, namespaces, and CDATA.
+
+## Basic use
+
+Without extra options, you get prettily printed XML with standard stuff:
+
+```cl
+(xmlw:writing-xml (*standard-output*)
+  (xmlw:with-tag ("foo")
+    (xmlw:attr "bar" "baz")
+    (xmlw:content "Hello world!")))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<foo bar="baz">
+  Hello world!
+</foo>
+```
+
+You can disable pretty printing and set the <?xml ...?> attributes with certain keyword arguments:
+
+```cl
+(xmlw:writing-xml (*standard-output* :indent nil :standalone t)
+  (xmlw:with-tag ("foo")
+    (xmlw:attr "bar" "baz")
+    (xmlw:content "Hello world!")))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?><foo bar="baz">Hello world!</foo>
+```
+
+Marking a tag compact removes the pretty printing from that tag, regardless of the whole document's settings:
+
+```cl
+(xmlw:writing-xml (*standard-output* :indent 4 :prologue nil)
+  (xmlw:with-tag ("foo")
+    (xmlw:attr "bar" "baz")
+    (xmlw:with-tag ("simple" :compact t)
+      (xmlw:content "simple content"))
+    (xmlw:with-tag ("complex")
+      (xmlw:cdata "extremely complex"))))
+```
+```xml
+<foo bar="baz">
+    <simple>simple content</simple>
+    <complex>
+	<![CDATA[extremely complex]]>
+    </complex>
+</foo>
+```
+
+For convenience, the previous example can be written shortly using the `tag` macro:
+
+```cl
+(xmlw:writing-xml (*standard-output* :indent 4 :prologue nil)
+  (xmlw:with-tag ("foo")
+    (xmlw:attr "bar" "baz")
+    (xmlw:tag "simple" "simple content")
+    (xmlw:with-tag ("complex")
+      (xmlw:cdata "extremely complex"))))
+```
+```xml
+<foo bar="baz">
+    <simple>simple content</simple>
+    <complex>
+	<![CDATA[extremely complex]]>
+    </complex>
+</foo>
+```
+
+Escaping of the special characters is done automatically:
+
+```cl
+(xmlw:writing-xml (*standard-output* :indent 4)
+  (xmlw:with-tag ("foo")
+    (xmlw:attr "bar" "i'm a so-called \"value\".")
+    (xmlw:content "42 > 17 && 17 < 42")
+    (xmlw:cdata "ASCII fish, like ]]>, taste good.")))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<foo bar="i&apos;m a so-called &quot;value&quot;.">
+    42 &gt; 17 &amp;&amp; 17 &lt; 42
+    <![CDATA[ASCII fish, like ]]]]><![CDATA[>, taste good.]]>
+</foo>
+```
+
+## Namespaces
+
+If you only need a namespace for a single tag, you can specify it there:
+
+```cl
+(xmlw:writing-xml (*standard-output*)
+  (xmlw:with-tag (((namespace "http://somwhere/quux") "foo"))
+    (xmlw:attr "bar" "baz")
+    (xmlw:content "Hello world!")))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<foo xmlns="http://somwhere/quux" bar="baz">
+  Hello world!
+</foo>
+```
+
+However, usually you would reuse the namespaces for a more complex structure:
+
+```cl
+(let ((quux (namespace "http://somwhere/quux"))
+      (xyzzy (namespace "http://somewhere/xyzzy")))
+  (xmlw:writing-xml (*standard-output*)
+    (xmlw:with-tag ((quux "foo"))
+      (xmlw:attr "bar" "baz")
+      (xmlw:with-tag ((xyzzy "greeting"))
+	(xmlw:content "Hello world!")))))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<foo xmlns="http://somwhere/quux" bar="baz">
+  <greeting xmlns="http://somewhere/xyzzy">
+    Hello world!
+  </greeting>
+</foo>
+```
+
+And to get the tags (and attributes) prefixed nicely:
+
+```cl
+(let ((quux (namespace "http://somwhere/quux"))
+      (xyzzy (namespace "http://somewhere/xyzzy")))
+  (xmlw:writing-xml (*standard-output*)
+    (xmlw:with-tag ((quux "foo"))
+      (xmlw:attr (xmlw:*xmlns* "quuxref") quux)
+      (xmlw:attr (xmlw:*xmlns* "xyzzy") xyzzy)
+      (xmlw:attr (quux "bar") "baz")
+      (xmlw:with-tag ((xyzzy "greeting"))
+	(xmlw:attr (quux "target") "mankind")
+	(xmlw:attr (xyzzy "kind") "benevolent")
+	(xmlw:content "Hello world!")))))
+```
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<quuxref:foo xmlns:quuxref="http://somwhere/quux" xmlns:xyzzy="http://somewhere/xyzzy" quuxref:bar="baz">
+  <xyzzy:greeting quuxref:target="mankind" xyzzy:kind="benevolent">
+    Hello world!
+  </greeting>
+</foo>
+```
+
+## Reference
+
+### Macro `WRITING-XML`
+
+**(writing-xml stream &key (version "1.0") (encoding "UTF-8") standalone (indent 2) (prologue t) &body body)**
+
+* *stream* (stream) specifies the stream for output
+* *version* (string) contains the value of the version attribute in the prologue
+* *encoding* (string) contains the vlaue of the encoding attribute in the prologue. Note that it does NOT have any effect on encoding of the content; that is left for the output stream to handle. Thus, you should match the value of the *encoding* parameter with your output stream's encoding.
+* if *standalone* (boolean) is set to a true value, the prologue will contain the `standalone="yes"` attribute
+* *indent* (`NIL` or integer) specifiees the number of spaces of indentation. Set to `NIL` to disable indentation and line breaks totally.
+* set *prologue* (boolean) to `NIL` to disable writing of the `<?xml ...?>` prologue.
+* The *body* contains the code for writing out the actual document.
+
+### Macro `WITH-TAG`
+
+**(with-tag (name &key compact) &body body)**
+**(with-tag ((ns name) &key compact) &body body)**
+
+* *name* (string) is the tag's name.
+* *ns* (namespace) is the tag's namespace.
+* *compact* (boolean) can be set to true to disable indentation and line feeds for this tag's content.
+* *body* contains the attributes, other tags, and content for the tag.
+
+### Macro `ATTR`
+
+**(attr name value)**
+**(attr (ns name) value)**
+
+* *name* (string) is the attribute's name.
+* *ns* (namespace) is the attribute's namespace.
+* *value* (string) is the attribute's value.
+
+### Function `CONTENT`
+
+**(content &rest data)**
+
+The *data* (strings) are written, escaped as necessary, into the document.
+
+### Function `CDATA`
+
+**(cdata &rest data)**
+
+The *data* (strings) are written, escaped as necessary, into the document, inside a `<![CDATA[...]]>` block.
+
+### Function `CONTENT-FORMAT`
+
+**(content-format &rest args)**
+
+A convenience function - identical to `(content (format nil [args]))`.
+
+### Function `CDATA-FORMAT`
+
+**(cdata-format &rest args)**
+
+A convenience function - identical to `(cdata (format nil [args]))`.
+
+### Macro `TAG`
+
+**(tag name &rest content)**
+**(tag (ns name) &rest content)**
+
+* *name* (string) is the tag's name.
+* *ns* (namespace) is the tag's namespace.
+* *content* (strings) contain the tag's content.
+
+### Macro `TAG-FORMAT`
+
+**(tag-format name &rest args)**
+**(tag-format (ns name) &rest args)**
+
+A convenience function - identical to `(tag name (format nil [args]))`.
---
+++ xmlw.lisp
@@ -0,0 +1,268 @@
+(in-package :xmlw)
+
+;;; The namespace class
+
+(defclass namespace ()
+  ((name :initarg :name :reader ns-name)))
+
+(defmethod print-object ((x namespace) stream)
+  (print-unreadable-object (x stream :type t :identity t)
+    (prin1 (ns-name x) stream)))
+
+(defmethod make-load-form ((ns namespace) &optional environment)
+  (declare (ignore environment))
+  `(make-instance 'namespace :name ,(ns-name ns)))
+
+(defun ns= (ns1 ns2)
+  (string= (ns-name ns1) (ns-name ns2)))
+
+(export 'namespace)
+(defun namespace (name)
+  (make-instance 'namespace :name name))
+
+(export '*xmlns*)
+(defparameter *xmlns* (namespace "xmlns")
+  "A namespace object denoting the 'xmlns' namespace.")
+
+;;; XML stream management
+
+(defvar *xml-stream*)
+(defvar *xml-pretty*)
+(defvar *xml-indent*)
+(defvar *xml-ns-dict*)
+
+(defun write-escaped (what &optional (stream *xml-stream*))
+  "Write a string into XML output, escaping the characters <>&\"' with XML entities as necessary."
+  (declare (optimize (speed 3) (space 0) (compilation-speed 0)))
+  (loop for ch across what
+	do (case ch
+	     (#\< (write-string "&lt;" stream))
+	     (#\> (write-string "&gt;" stream))
+	     (#\& (write-string "&amp;" stream))
+	     (#\" (write-string "&quot;" stream))
+	     (#\' (write-string "&apos;" stream))
+	     (otherwise (write-char ch stream)))))
+
+(defun write-name (n &optional (stream *xml-stream*))
+  "Write a name - either a simple string or a namespace/string combination. Returns T if a fully qualified name has been written out, NIL if there is no namespace alias in the dictionary."
+  (if (listp n)
+      (let ((entry (find (first n) *xml-ns-dict* :key #'car)))
+	(if entry
+	    (prog1 t
+	      (write-string (cdr entry) stream)
+	      (write-char #\: stream)
+	      (write-string (second n) stream))
+	    (prog1 nil
+	      (write-string (second n) stream))))
+      (prog1 t
+	(write-string n *xml-stream*))))
+
+(defun write-attr (name value &optional (stream *xml-stream*))
+  "Writes the name and value of an attribute."
+  (write-char #\space stream)
+  (unless (write-name name stream)
+    (error "Namespace ~s not defined as alias, yet used for an attribute" (first name)))
+  (write-char #\= stream)
+  (write-char #\" stream)
+  (write-escaped value stream)
+  (write-char #\" stream))
+
+(defun indent (delta &optional (stream *xml-stream*))
+  "Indents, if necessary, to the correct indentation level."
+  (when *xml-pretty*
+    (multiple-value-bind (tabs spaces)
+	(floor (+ *xml-indent* (* delta *xml-pretty*)) 8)
+      (dotimes (i tabs)
+	(write-char #\tab stream))
+      (dotimes (i spaces)
+	(write-char #\space stream)))))
+
+;;; The XML output interface
+
+(defvar *current-tag-name*)
+(defvar *current-tag-attrs*)
+(defvar *current-tag-opened*)
+(defvar *current-tag-compact*)
+
+(export 'writing-xml)
+(defmacro writing-xml ((stream &key (version "1.0") (encoding "UTF-8") standalone (indent 2) (prologue t))
+		       &body body)
+  "Main wrapper for writing out XML data. Writes the <?xml...?> tag followed by the XML document in body."
+  `(let ((*xml-stream* ,stream)
+	 (*xml-pretty* ,indent)
+	 (*xml-indent* (and ,indent 0))
+	 (*xml-ns-dict* `((,*xmlns* . "xmlns")))
+	 (*current-tag-compact* ,(not indent)))
+     (when ,prologue
+       (write-string "<?xml" *xml-stream*)
+       (write-attr "version" ,version)
+       (write-attr "encoding" ,encoding)
+       (when ,standalone
+	 (write-attr "standalone" "yes"))
+       (write-string "?>" *xml-stream*)
+       (when *xml-pretty*
+	 (terpri *xml-stream*)))
+     ,@body))
+
+(defun finish-open-tag (&key close)
+  "After collecting the necessary information about a tag being opened, write it out now."
+  (unless *current-tag-opened*
+    (indent -1)
+    (write-char #\< *xml-stream*)
+    (unless (write-name *current-tag-name*)
+      (write-attr "xmlns" (ns-name (first *current-tag-name*))))
+    (dolist (a (nreverse *current-tag-attrs*))
+      (write-attr (car a) (cdr a)))
+    (if close
+	(write-string " />" *xml-stream*)
+	(write-char #\> *xml-stream*))
+    (when *current-tag-compact*
+      (setf *xml-pretty* nil))
+    (when *xml-pretty*
+      (terpri *xml-stream*))
+    (setf *current-tag-opened* t)))
+
+(export 'with-tag)
+(defmacro with-tag ((name &key compact) &body body)
+  "Write out a tag. If compact is specified, the tag and all its contents will be written without line feeds and indentation also when pretty printing is enabled for the XML document."
+  (when (and (listp name)
+	     (or (not (first name))
+		 (not (second name))
+		 (cddr name)))
+    (error 'type-error :expected-type '(list namespace string) :datum name))
+  (let ((was-pretty (gensym)))
+    `(progn
+       (when (boundp '*current-tag-opened*)
+	 (finish-open-tag))
+       ,@(when (listp name)
+	       `((unless (typep ,(first name) 'namespace)
+		   (error 'type-error :expected-type 'namespace :datum ,(first name)))
+		 (unless (stringp ,(second name))
+		   (error 'type-error :expected-type 'string :datum ,(second name)))))
+       (let ((*current-tag-name* ,(if (listp name)
+				      `(list ,@name)
+				      name))
+	     (*current-tag-attrs* nil)
+	     (*current-tag-opened* nil)
+	     (*current-tag-compact* (or *current-tag-compact* ,compact))
+	     (*xml-indent* (and *xml-pretty* (+ *xml-indent* *xml-pretty*)))
+	     (,was-pretty *xml-pretty*)
+	     (*xml-pretty* (and (not *current-tag-compact*) *xml-pretty*)))
+	 (multiple-value-prog1
+	     (progn ,@body)
+	   (if *current-tag-opened*
+	       (progn
+		 (indent -1)
+		 (write-string "</" *xml-stream*)
+		 (write-string ,(if (listp name)
+				    (second name)
+				    name)
+			       *xml-stream*)
+		 (write-char #\> *xml-stream*)
+		 (when ,was-pretty
+		   (terpri *xml-stream*)))
+	       (finish-open-tag :close t)))))))
+
+(defun attr% (ns name value)
+  "Helper function for writing attribute data."
+  (unless (typep ns 'namespace)
+    (error 'type-error :expected-type 'namespace :datum ns))
+  (if (eq ns *xmlns*)
+      (if (typep value 'namespace)
+	  (progn
+	    (push (cons value name) *xml-ns-dict*)
+	    (push (cons (list ns name) (ns-name value)) *current-tag-attrs*))
+	  (error 'type-error :expected-type 'namespace :datum value))
+      (if (stringp name)
+	  (push (cons (list ns name) value) *current-tag-attrs*)
+	  (error 'type-error :expected-type 'string :datum name)))
+  value)
+
+(export 'attr)
+(defmacro attr (name value)
+  "Write an attribute definition to the current tag."
+  (when (and (listp name)
+	     (or (not (first name))
+		 (not (second name))
+		 (cddr name)))
+    (error 'type-error :expected-type '(list namespace string) :datum name))
+  `(if *current-tag-opened*
+       (error "attribute definitions must precede child tags (tag ~a)" *current-tag-name*)
+       ,(if (listp name)
+	    `(attr% ,(first name) ,(second name) ,value)
+	    `(prog1 ,value
+	       (push (cons ,name ,value) *current-tag-attrs*)))))
+
+(export 'content)
+(defun content (&rest data)
+  "Write content to the current tag."
+  (finish-open-tag)
+  (indent 0)
+  (dolist (i data)
+    (unless (stringp i)
+      (error 'type-error :expected-type 'string :datum i))
+    (write-escaped i))
+  (when *xml-pretty*
+    (terpri *xml-stream*))
+  (first (last data)))
+
+(export 'content-format)
+(defun content-format (&rest args)
+  "Write formatted content to the current tag."
+  (content (apply #'format nil args)))
+
+(export 'tag)
+(defmacro tag (name &rest content)
+  "Write a compact tag with the given content."
+  `(with-tag (,name :compact t)
+     (content ,@content)))
+
+(export 'cdata)
+(defun cdata (&rest data)
+  "Write a CDATA block."
+  (finish-open-tag)
+  (indent 0)
+  (write-string "<![CDATA[" *xml-stream*)
+  (let ((esc 0))
+    (dolist (i data)
+      (unless (stringp i)
+	(error 'type-error :expected-type 'string :datum i))
+      (cond ((and (= esc 1)
+		  (>= (length i) 2)
+		  (string= (subseq i 0 2) "]>"))
+	     (write-string "]]]><![CDATA[>" *xml-stream*)
+	     (setf i (subseq i 2)))
+	    ((and (= esc 2)
+		  (>= (length i) 1)
+		  (char= (char i 0) #\>))
+	     (write-string "]]><![CDATA[" *xml-stream*)))
+      (let ((start 0))
+	(loop
+	   (let ((next (search "]]>" i :start2 start)))
+	     (if next
+		 (progn
+		   (incf next 2)
+		   (write-string i *xml-stream* :start start :end next)
+		   (write-string "]]><![CDATA[" *xml-stream*)
+		   (setf start next))
+		 (progn
+		   (write-string i *xml-stream* :start start)
+		   (return))))))
+      (setf esc
+	    (cond ((and (>= (length i) 2)
+			(string= i "]]" :start1 (- (length i) 2)))
+		   2)
+		  ((and (>= (length i) 1)
+			(char= #\] (char i (1- (length i)))))
+		   1)
+		  (t
+		   0)))))
+  (write-string "]]>" *xml-stream*)
+  (when *xml-pretty*
+    (terpri *xml-stream*))
+  (first (last data)))
+
+(export 'cdata-format)
+(defun cdata-format (&rest args)
+  "Write a CDATA block, filling it with formatted data."
+  (cdata (apply #'format nil args)))
